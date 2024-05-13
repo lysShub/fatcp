d43c9689c1469188b975aa92b17bac8762f9cd02
lysShub <lysShub@gmail.com>
2024-05-13 17:43:46 +0800

    fix unit test

--- handshake_test.go
+++ handshake_test.go
@@ -158,7 +158,9 @@ func Test_Handshake_Context_Cancel(t *testing.T) {
 			conn, err := NewConn(c, cfg)
 			require.NoError(t, err)
 			_, err = conn.BuiltinTCP(ctx)
-			require.Contains(t, []error{os.ErrDeadlineExceeded, context.Canceled}, err, err)
+
+			ok := errors.Is(err, os.ErrDeadlineExceeded) || errors.Is(err, context.Canceled)
+			require.True(t, ok, err)
 			return nil
 		})
 
