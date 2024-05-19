6a109514dd138bb987d004f51fd0c355e339bc78
lysShub <lysShub@gmail.com>
2024-05-19 20:36:52 +0800

    fix mock attacher

--- attacher_test.go
+++ attacher_test.go
@@ -1,6 +1,8 @@
 package fatcp
 
 import (
+	"errors"
+
 	"github.com/lysShub/netkit/packet"
 )
 
@@ -13,13 +15,29 @@ type mocker struct {
 
 var _ Attacher = &mocker{}
 
-func (m *mocker) Builtin() Attacher               { return &mocker{valid: true, builtin: true} }
-func (m *mocker) IsBuiltin() bool                 { return m.builtin }
-func (m *mocker) Valid() bool                     { return m.valid }
-func (m *mocker) Overhead() int                   { return 0 }
-func (m *mocker) String() string                  { return "mocker" }
-func (m *mocker) Encode(pkt *packet.Packet) error { return nil }
+func (m *mocker) Builtin() Attacher { return &mocker{valid: true, builtin: true} }
+func (m *mocker) IsBuiltin() bool   { return m.builtin }
+func (m *mocker) Valid() bool       { return m.valid }
+func (m *mocker) Overhead() int     { return 1 }
+func (m *mocker) String() string    { return "mocker" }
+func (m *mocker) Encode(pkt *packet.Packet) error {
+	if m.builtin {
+		pkt.Attach([]byte{1})
+	} else {
+		pkt.Attach([]byte{0})
+	}
+	return nil
+}
 func (m *mocker) Decode(pkt *packet.Packet) error {
-	m.valid = true
+	b := pkt.Detach(make([]byte, 1))
+	if b[0] == 1 {
+		m.builtin = true
+		m.valid = true
+	} else if b[0] == 0 {
+		m.builtin = false
+		m.valid = true
+	} else {
+		return errors.New("invalid  packet")
+	}
 	return nil
 }
