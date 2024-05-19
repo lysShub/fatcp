68961db563e6d99e3e198b2c155bc4e024452e2d
lysShub <lysShub@gmail.com>
2024-05-19 20:19:13 +0800

    fix mocker

--- attacher_test.go
+++ attacher_test.go
@@ -15,7 +15,7 @@ var _ Attacher = &mocker{}
 
 func (m *mocker) Builtin() Attacher               { return &mocker{valid: true, builtin: true} }
 func (m *mocker) IsBuiltin() bool                 { return m.builtin }
-func (m *mocker) Valid() bool                     { return m.builtin }
+func (m *mocker) Valid() bool                     { return m.valid }
 func (m *mocker) Overhead() int                   { return 0 }
 func (m *mocker) String() string                  { return "mocker" }
 func (m *mocker) Encode(pkt *packet.Packet) error { return nil }
